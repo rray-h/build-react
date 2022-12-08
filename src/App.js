e9d13dc5faa33fb223cd33cc62aea6fb4a6cf37e
rray-h <114110100+rray-h@users.noreply.github.com>
2022-12-08 14:50:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="container">
+        <h1><a href="#">React</a></h1>
+        <nav className="menu">
+          <ul className="list">
+            <li className="item"><a href="#" className="link">Home</a></li>
+            <li className="item"><a href="#" className="link">Contact</a></li>
+            <li className="item"><a href="#" className="link">Help</a></li>
+            <li className="item"><a href="#" className="link">File</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
